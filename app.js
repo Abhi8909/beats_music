78319bcbd534a49c9151e31189bca8fa3172c0fb
abhishek gupta <abhishekgupta8909@gmail.com>
2020-01-26 13:29:58 +0530

    routes defind

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 let http = require("http");
+let routes = require("./routes");
 
 http
   .createServer(function(req, res) {
-    res.end("Hello World\n");
+    if (routes[req.pathname])
+      routes[req.pathname]({ abhishek: "gupta" }, () => {
+        res.end("Hello World\n");
+      });
+    else res.end("Not found");
   })
   .listen(3000, () => {
     console.log("Server is listening at port at 3000");
